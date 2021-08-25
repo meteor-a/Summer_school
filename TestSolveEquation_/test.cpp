da30f420c0def8efacb561f5457babb1113b1cf1
meteor-a <61750320+meteor-a@users.noreply.github.com>
2021-08-25 20:47:02 +0300

    tests more

--- TestSolveEquation_/test.cpp
+++ TestSolveEquation_/test.cpp
@@ -2,6 +2,7 @@
 #include "../Summer_school/Summer_school.h"
 #include <fstream>
 #include <cstdlib>
+#include <math.h> 
 
 TEST(TestCaseName, TestName) {
     EXPECT_EQ(1, 1);
@@ -10,28 +11,36 @@ TEST(TestCaseName, TestName) {
     if (!file.is_open()) {
         int tt = 0;
     }
+    int num_of_tests = 0;
+    file >> num_of_tests;
+    for (int ii = 0; ii < num_of_tests; ++ii) {
+        double a_test = 0;
+        double b_test = 0;
+        double c_test = 0;
+        int nRoots_test = 0;
+        double x1_test = 0;
+        double x2_test = 0;
 
-    double a_test = 0;
-    double b_test = 0;
-    double c_test = 0;
-    int nRoots_test = 0;
-    double x1_test = 0;
-    double x2_test = 0;
+        file >> a_test >> b_test >> c_test >> nRoots_test;
+        if (nRoots_test == 1) {
+            file >> x1_test;
+        }
+        else if (nRoots_test == 2) {
+            file >> x1_test;
+            file >> x2_test;
+        }
 
-    file >> a_test >> b_test >> c_test >> nRoots_test;
-    if (nRoots_test == 1) {
-        file >> x1_test;
+        double x1 = 0;
+        double x2 = 0;
+        int nRoots = SolveSquare(a_test, b_test, c_test, &x1, &x2);
+
+        EXPECT_EQ(nRoots, nRoots_test);
+        if (nRoots == 1) {
+            EXPECT_TRUE(is_equal(x1, x1_test));
+        }
+        else {
+            EXPECT_TRUE(is_equal(x1, x1_test) && is_equal(x2, x2_test));
+        }
+        
     }
-    else if (nRoots_test == 2) {
-        file >> x1_test;
-        file >> x2_test;
-    }
-
-    double x1 = 0;
-    double x2 = 0;
-    int nRoots = SolveSquare(a_test, b_test, c_test, &x1, &x2);
-    
-    EXPECT_EQ(nRoots, nRoots_test);
-    EXPECT_DOUBLE_EQ(x1_test, x1);
-    EXPECT_DOUBLE_EQ(x2, x2_test);
 }
